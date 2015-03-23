6cd3bef7fb609a8ef880cd7cd8379439fb9fb8a6
bumbum <bumbum@cooperativa.cat>
2015-03-23 23:09:35 +0100

    update the api to accept /api/ticker and return a json with 4 fiat currency rates (EUR,USD,GBP,CHF)...

--- api.php
+++ api.php
@@ -4,7 +4,7 @@ require_once('../../../wp-config.php');
 
 function fair_price(){
   $parts = explode('/', $_SERVER['REQUEST_URI']);
-  if($parts[2] == 'fair-eur' || $parts[2] == 'eur-fair'){
+  if($parts[2] == 'ticker' || $parts[2] == 'fair-eur' || $parts[2] == 'eur-fair'){
     global $edd_options;
     if(!isset($edd_options)){
       $price = get_option('faircoin_price')*1;
@@ -18,14 +18,33 @@ function fair_price(){
     if($parts[2] == 'fair-eur'){
 
       header('Content-type: application/json');
-      echo json_encode(array('fair-eur'=>$price));
+      echo json_encode(array('eur-fair'=>$price*1));
 
     } else if($parts[2] == 'eur-fair'){
 
       header('Content-type: application/json');
       echo json_encode(array('eur-fair'=>number_format((1/$price), 8, '.', '')*1));
 
+    } else if($parts[2] == 'ticker'){
+
+      //$edd_currencies = edd_get_currencies();
+      $exchange_rates  = edd_currency_get_exchange_rates();
+      $eur2usd = $exchange_rates['EUR'];
+      $price_USD = $price * $eur2usd;
+      $price_GBP = $price_USD / $exchange_rates['GBP'];
+      $price_CHF = $price_USD / $exchange_rates['CHF'];
+
+      header('Content-type: application/json');
+      echo json_encode(
+        array(
+            'EUR' => array('last' => number_format((1/$price), 8, '.', '')*1),
+            'USD' => array('last' => number_format((1/$price_USD), 8, '.', '')*1),
+            'GBP' => array('last' => number_format((1/$price_GBP), 8, '.', '')*1),
+            'CHF' => array('last' => number_format((1/$price_CHF), 8, '.', '')*1),
+        )
+      );
     }
+
   } else {
     echo 'No var requested! (try api/fair-eur or api/eur-fair) the var '.$parts[2].' is not understood.';
   }
